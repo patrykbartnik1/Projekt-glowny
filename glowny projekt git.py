f03113bea09b9050865632396bcccb18e7ae64ef
patrykbartnik1 <patryk.bartnik1@op.pl>
2025-03-22 07:30:15 +0100

    Add files via upload

--- glowny projekt git.py
+++ glowny projekt git.py
@@ -1,8 +1,36 @@
-# aplikacja do zarzadzania domowym budzetem
-# zalozenia aplikacji:
-# wewnetrzny system z rejestracja i logowaniem
-# dodawanie wydatkow i przychodow, zmiana stanu konta
-# kategoryzacja wydatkow (jedzenie, transport itp)
-# generowanie raportow miesiecznych
-# eksport danych do pliku pdf(reportlab)/csv
-# generowanie wykresow
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///budget.db'
+app.config['SECRET_KEY'] = 'tajny_klucz_do_formularzy'
+db = SQLAlchemy(app)
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(10))
+    category = db.Column(db.String(50))
+    amount = db.Column(db.Float)
+    description = db.Column(db.String(200))
+
+@app.route('/')
+def index():
+    transactions = Transaction.query.all()
+    return render_template('index.html', transactions=transactions)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        t_type = request.form['type']
+        category = request.form['category']
+        amount = float(request.form['amount'])
+        description = request.form['description']
+        new_transaction = Transaction(type=t_type, category=category, amount=amount, description=description)
+        db.session.add(new_transaction)
+        db.session.commit()
+        return redirect('/')
+    return render_template('add.html')
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
